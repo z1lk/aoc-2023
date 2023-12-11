5672d9ac738b0f0d9521fb76893160c5c5d84741
z1lk <z1lk@pm.me>
2023-12-11 08:28:17 -0600

    Refactor to use part 2 solution for part 1.
    Optimize by using iterators instead  of eagerly casting to arrays

--- src/solutions/11.cr
+++ src/solutions/11.cr
@@ -1,53 +1,29 @@
 class Aoc2023::Eleven < Aoc2023::Solution
-  def parse(lines)
-    # convert to chars so we can transpose
-    lines = lines.map { |l| l.chars }
-    lines2 = [] of Array(Char)
-    lines.each do |l|
-      lines2 << l
-      lines2 << l if l.all? { |c| c == '.' }
-    end
-    lines2 = lines2.transpose
-    lines3 = [] of Array(Char)
-    lines2.each do |l|
-      lines3 << l
-      lines3 << l if l.all? { |c| c == '.' }
-    end
-    map = Map.new(lines3.transpose)
+  def solve(lines, expansion)
+    map = Parsers.map(lines)
+    empty_x = 0.to(map.max_x).select do |x|
+      0.to(map.max_y).all? do |y|
+        map.get({x: x, y: y}) == '.'
+      end
+    end.to_a
+    empty_y = 0.to(map.max_y).select do |y|
+      0.to(map.max_x).all? do |x|
+        map.get({x: x, y: y}) == '.'
+      end
+    end.to_a
+    map.find_all('#').each_combination(2, true).map do |(a,b)|
+      d = map.dist(a,b).to_i64
+      d += (expansion - 1) * empty_x.count { |n| n.in?(a[:x].to(b[:x])) }
+      d += (expansion - 1) * empty_y.count { |n| n.in?(a[:y].to(b[:y])) }
+      d
+    end.sum
   end
 
   def part1(lines)
-    map = parse lines
-    galaxies = map.find_all('#')
-    galaxies.combinations(2).sum do |(a,b)|
-      map.dist(a,b)
-    end
+    solve lines, 2
   end
 
   def part2(lines)
-    map = Parsers.map(lines)
-    empty_cols = [] of Int32
-    empty_rows = [] of Int32
-    map.max_x.times do |x|
-      chars = 0.to(map.max_y).to_a.map do |y|
-        map.get({x: x, y: y})
-      end
-      empty_cols << x if chars.all? { |c| c == '.' }
-    end
-    map.max_y.times do |y|
-      chars = 0.to(map.max_x).to_a.map do |x|
-        map.get({x: x, y: y})
-      end
-      empty_rows << y if chars.all? { |c| c == '.' }
-    end
-    galaxies = map.find_all('#')
-    galaxies.combinations(2).sum do |(a,b)|
-      d = map.dist(a,b).to_i64
-      n_empty_cols_crossed = a[:x].to(b[:x]).to_a.&(empty_cols).size
-      n_empty_rows_crossed = a[:y].to(b[:y]).to_a.&(empty_rows).size
-      d += (1_000_000 * n_empty_cols_crossed) - n_empty_cols_crossed
-      d += (1_000_000 * n_empty_rows_crossed) - n_empty_rows_crossed
-      d
-    end
+    solve lines, 1_000_000
   end
 end
